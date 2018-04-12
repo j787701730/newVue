bb1c7d2845a52774699ad2a497911e7cc6b1ebbb
zhengw <247276359@qq.com>
2018-04-12 17:25:48 +0800

    添加一个页面

---
+++ src/components/detail/Detail.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <!-- <h1>{{ msg }}</h1> -->
+    <!-- <h2>{{id}}</h2> -->
+    <h2>我是详情页</h2>
+    <router-link to="/">Go to 主页</router-link>
+
+
+  </div>
+</template>
+
+<script>
+// import axios from "axios";
+export default {
+  name: "Detail",
+  data() {
+    return {
+      msg: "Detail",
+      // id: this.$route.params.id
+      // id: this.$route.query.a,
+      books: []
+    };
+  },
+  mounted: function() {
+    // var self = this;
+
+    // axios
+    //   .post("https://www.zhengw.top/getBooks", {
+    //     firstName: "Fred",
+    //     lastName: "Flintstone"
+    //   })
+    //   .then(function(response) {
+    //     console.log(response.data);
+    //     self.books = response.data;
+    //   })
+    //   .catch(function(error) {
+    //     console.log(error);
+    //   });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Index from '@/components/Index'
 import Books from '@/components/Books'
 import Error from '@/components/Error'
+import Detail from '@/components/detail/Detail'
 // import config from '@/config/config'
 
 Vue.use(Router)
@@ -35,6 +36,17 @@ export default new Router({
       // console.log(config.a)
       // console.log(config.bb)
       // console.log(config.clear(10))
+      document.title = to.meta['title']
+      next()
+    }
+  }, {
+    path: '/detail',
+    name: 'Detail',
+    component: Detail,
+    meta: {
+      title: '详情页'
+    },
+    beforeEnter: (to, from, next) => {
       document.title = to.meta['title']
       next()
     }
